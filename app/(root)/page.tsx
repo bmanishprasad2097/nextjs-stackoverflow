5854513c60caa56f4db16086a1607e7956d2434f
Manish Prasad <prasad.bmanish999@gmail.com>
2024-04-05 18:52:26 +0530

    Added Tailwind config and global css

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -4,6 +4,9 @@ import { UserButton } from "@clerk/nextjs";
 export default function Home() {
   return (
     <div>
+      <div className="h1-bold">Main Content</div>
+      <div className="h2-bold">Main Content</div>
+      <div className="h3-bold">Main Content</div>
       <UserButton afterSignOutUrl="/" />      
     </div>
   );
